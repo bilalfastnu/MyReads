74f18233b05a2f16fb93837801c6bd9fece02a11
Muhammad Bilal <muhammadbilal.fastnu@gmail.com>
2017-07-28 00:14:34 -0400

    Add onChange to "Move to" select element

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -7,12 +7,18 @@ class ShelfChanger extends Component {
     book: PropTypes.object.isRequired
   }
 
+  state =  {  value: 'Move to...'}
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+    console.log("change to", this.state.value)
+  }
   render() {
     const { book } = this.props
 
     return (
       <div className="book-shelf-changer">
-        <select>
+        <select value={this.state.value} onChange={this.handleChange}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
