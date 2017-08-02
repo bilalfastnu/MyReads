a201e7758108cddc2348a315bf8ad5a07598480e
Muhammad Bilal <muhammadbilal.fastnu@gmail.com>
2017-08-02 17:16:39 -0400

    Create array for shelf properties

--- src/BookList.js
+++ src/BookList.js
@@ -5,61 +5,47 @@ import BookShelf from './BookShelf'
 class BookList extends Component {
   static propTypes = {
     books: PropTypes.array.isRequired,
-  //  shelfChange: PropTypes.bool.isRequired,
-  //  notifyShelfChange: PropTypes.func.isRequired
   }
 
   state = { shelfChange: false }
 
   onShelfChange() {
     this.setState({ shelfChange: true })
-  //  this.props.notifyShelfChange();
   }
 
   render() {
     const { books } = this.props
-    let currentBooks = books.filter( book => book.shelf === "currentlyReading")
-    let wantToBooks =  books.filter( book => book.shelf === "wantToRead")
-    let readBooks = books.filter( book => book.shelf === "read")
+    const shelfTypes = [{
+      type: 'currentlyReading',
+      title: 'Currently Reading'
+    },{
+      type: 'wantToRead',
+      title: 'Want to Read'
+    },{
+      type: 'read',
+      title: 'Read'
+    },
+  ]
 
     return (
       <div className="list-books-content">
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <BookShelf
-                books={ currentBooks }
-                shelfChange={this.state.shelfChange}
-                notifyShelfChange={() => this.onShelfChange() }
-              />
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <BookShelf
-                books={ wantToBooks }
-                shelfChange={this.state.shelfChange}
-                notifyShelfChange={() => this.onShelfChange() }
-              />
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <BookShelf
-                books={ readBooks }
-                shelfChange={this.state.shelfChange}
-                notifyShelfChange={() => this.onShelfChange() }
-              />
-            </div>
-          </div>
-        </div>
+        {shelfTypes.map((shelf, index) =>  {
+          const shelfBooks = books.filter( book => book.shelf === shelf.type)
+          return  (
+            <div className="bookshelf" key={index}>
+              <h2 className="bookshelf-title">{ shelf.title }</h2>
+              <div className="bookshelf-books">
+                <BookShelf
+                  books={ shelfBooks }
+                  shelfChange={this.state.shelfChange}
+                  notifyShelfChange={() => this.onShelfChange() }
+                />
+              </div>
+            </div> )
+        })}
       </div>
     )
   }
-
 }
 
 export default BookList
